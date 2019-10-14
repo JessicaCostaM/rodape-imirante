7d97475d092086c928596ad35692f0a0db1d47e3
Jéssica Costa <jessica-costa-rt@hotmail.com>
2019-10-14 10:46:15 -0300

    Rodapé Portal de Notícias

---
+++ footer.php
@@ -0,0 +1,49 @@
+		
+		<footer class="rodape">	
+			<!--Esta parte contém o conteúdo dos links do rodapé-->
+			<div class="rodape-topo clearfix">
+				<div class="menu-footer">
+					<a class="item" href="https://imirante.com/plantaoi">NOTÍCIAS</a>
+					<a class="item " href="https://imirante.com/esporte">ESPORTES</a>
+					<a class="item" href="https://imirante.com/namira">NA MIRA</a>
+					<a class="item" href="https://imirante.com/namira/culinaria">CULINÁRIA</a>
+					<a class="item" href="https://imirante.com/blogs">BLOGS</a>
+					<a class="item" href="https://imirante.com/oestadoma">O ESTADO</a>
+					<a class="item" href="https://imirante.com/miranteam">MIRANTE AM</a>
+					<a class="item" href="https://imirante.com/mirantefm">MIRANTE FM</a>
+				</div>
+			</div>
+
+			<!--Esta parte contém o conteúdo da logo do Imirante e redes sociais-->
+			<div class="rodape-meio">
+				<div class="navbar navbar-dark">
+					<div class="col-12 rodape__logo">
+						<div class="texto-portal">
+							<img src="https://assets.imirante.com/3.1/imagens/logo-imirante-topo-desktop.svg">
+							<p class="text-white text-center"> O Portal do Maranhão </p>
+						</div>
+					</div>
+					<div class="col">
+						<div class="rodape__redes" >
+							<div class="rodape__social">
+								<a href="http://fb.com/portalimirante" target="_blank" class="rodape__socialItem rodape__socialItem--facebook"></a>
+								<a href="http://twitter.com/imirante" target="_blank" class="rodape__socialItem rodape__socialItem--twitter"></a>
+								<a href="http://instagr.am/imirante" target="_blank" class="rodape__socialItem rodape__socialItem--instagram"></a>
+							</div>							
+						</div>
+						<div class="rodape__whatsapp">
+							<a target="_blanck" href="http://api.whatsapp.com/send?phone=5598992092383">
+								<p class="item">(98) 99209-2383</p>
+							</a>									
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<!--Esta parte contém o conteúdo da logo do Imirante e redes sociais-->
+			<div class="rodape-baixo">
+				<div class="rodape__direitos" >
+					<a> © 2019 - Todos os direitos reservados. </a>
+				</div>
+			</div>
+		</footer>
